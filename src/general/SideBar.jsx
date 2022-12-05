77cc776f1a565438ff5b56df0c4a671fad456649
Nish Lingam <nish.gnanalingam@nationwide.co.uk>
2022-12-05 16:48:12 +0000

    Created MVP DiaryPage

--- src/general/SideBar.jsx
+++ src/general/SideBar.jsx
@@ -2,13 +2,12 @@ import React from 'react';
 
 import SideBarButton from './SideBarButton';
 
-const SideBar = ({ sideBarActive }) => {
-    
-    const todayDate = new Date().toJSON().slice(0, 10);;
+const SideBar = ({ sideBarActive, todayDate }) => {
+
     const buttonTexts = [todayDate, "Timeline", "Download", "Upgrade", "Settings", "Contact Us"]
 
     return (
-        <div className= {`max-h-screen w-48 bg-yellow-300 flex-1 bg-opacity-80 duration-300 ${sideBarActive ? "active-side-bar" : "inactive-side-bar"}`}>
+        <div className= {`w-48 mr-6 bg-yellow-300 flex-initial bg-opacity-80 duration-300 ${sideBarActive ? "active-side-bar" : "inactive-side-bar"}`}>
                 {buttonTexts.map(i => <SideBarButton buttonText={i}/>)}
         </div>
     )
